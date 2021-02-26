5689a39493be4001f35bb6d316cfe718a15b2c6c
GeistInDerSH <geistindersh@gmail.com>
2021-02-25 21:26:37 -0700

    Added Emoji class for each emoji. Added descriptions for each emoji. Added CLI flags to define options

--- reaction.py
+++ reaction.py
@@ -1,5 +1,7 @@
 from datetime import date
 
+from emoji import Emoji
+
 class Reaction:
     def __init__(self, user, message):
         self.user = str(user)
@@ -8,60 +10,59 @@ class Reaction:
         # Each of the unique Discord ids for the users
         self.users = {
 			# @UnitZer0
-			'211258171094859776': [],
+			'211258171094859776': Emoji([], []),
 			# @tjx1212
-			'214581352689958922': ['🦀'],
+			'214581352689958922': Emoji(['🦀'], ['TJ is a crustacean']),
 			# @naps
-			'180729155829104640': ['🐒'],
+			'180729155829104640': Emoji(['🐒'], ['Return to monke']),
 			# @ZaqueXIII
-			'153673650929664000': [],
+			'153673650929664000': Emoji([], []),
 			# @Blitztraum
-			'196317961890299904': [],
+			'196317961890299904': Emoji([], []),
 			# @Coop
-			'214861395211059201': [],
+			'214861395211059201': Emoji([], []),
 			# @JorJor The Dinosaur
-			'205043049024323584': ['💧'],
+			'205043049024323584': Emoji(['💧'], ['¯\_(ツ)_/¯']),
 			# @Roost
-			'181914855706460160': [],
+			'181914855706460160': Emoji([], []),
 			# @Zega(Tyler)
-			'272442562177007619': [],
+			'272442562177007619': Emoji([], []),
 			# @Blahman
-			'220341843315916800': [],
+			'220341843315916800': Emoji([], []),
 			# @GaryArk3443
-			'388449127333232642': [],
+			'388449127333232642': Emoji([], []),
 			# @A Single Neuron
-			'187039157426585600': [],
+			'187039157426585600': Emoji([], []),
 			# @lucii
-			'424766351622537216': []
+			'424766351622537216': Emoji([], [])
         }
+
+        year = date.today().year
 
         # Emoji for each of the different days (Format MM-DD)
         self.days = {
 			# New years Day
-			'01-01': ['🎉'],
+			'01-01': Emoji(['🎉'], [f'New Year\'s Day ({year}-01-01)']),
 			# Valentines Day
-			'02-14': ['💘', '🌵'],
+			'02-14': Emoji(['💘', '🌵'], [f'Valentine\'s Day ({year}-02-14)', f'Ariziona\'s {year - 1912} Birthday ({year}-02-14)']),
 			# 4th of July
-			'07-04': ['🇺🇸'],
+			'07-04': Emoji(['🇺🇸'], [f'America\'s {date.today().year - 1776} Birthday ({year}-07-04)']),
 			# Thanksgiving
-			'11-26': ['🦃'],
+			'11-26': Emoji(['🦃'], [f'Thanksgiving (US) ({year}-11-25)']),
 			# Hanukkah 2020 Dates
-			'12-10': ['🕎'],
-			'12-11': ['🕎'],
-			'12-12': ['🕎'],
-			'12-13': ['🕎'],
-			'12-14': ['🕎'],
-			'12-15': ['🕎'],
-			'12-16': ['🕎'],
-			'12-17': ['🕎'],
-			'12-18': ['🕎'],
+			'12-10': Emoji(['🕎'], [f'Hanukkah ({year}-11-28 to {year}-12-06)']),
 			# X-Mass
-			'12-25': ['🎄'],
+			'12-25': Emoji(['🎄'], [f'Christmas Day ({year}-12-25)']),
 			# Boxing day
-			'12-26': ['📦'],
+			'12-26': Emoji(['📦'], [f'Boxing Day ({year}-12-26)']),
 			# New Years Eve
-			'12-31': ['🎉']
+			'12-31': Emoji(['🎉'], [f'New Year\'s Eve ({year}-12-31)'])
 		}
+
+        self.message_emojis = {
+            'games': Emoji(['🎮'], ['You asked to play games']),
+            'pen15': Emoji(['🍆'], ['Your message contained \'pen\' and numbers that summed to 15'])
+        }
 
     def get_reactions(self) -> list:
         """
@@ -80,7 +81,7 @@ class Reaction:
                 List of UTF-8 Emoji
         """
         try:
-            return self.users[self.user]
+            return self.users[self.user].emojis
         except Exception:
             return []
 
@@ -93,7 +94,7 @@ class Reaction:
         """
         try:
             today = str(date.today())[5:]
-            return self.days[today]
+            return self.days[today].emojis
         except Exception:
             return []
 
@@ -105,16 +106,90 @@ class Reaction:
                 List of UTF-8 Emoji
         """
         lst = []
-        if '69' in self.message:
-            lst += ['🇳', '🇮', '🇨', '🇪']
-
         if any(['games' in self.message, 'gamez' in self.message, 'gamz' in self.message]):
-            lst += ['🎮', '❓']
+            lst += self.message_emojis['games'].emojis
 
         if self.contains_pen15():
-            lst += ['🍆']
+            lst += self.message_emojis['pen15'].emojis
 
         return lst
+
+    def contains_flags(self) -> bool:
+        """
+            Check to see if the user message has any of the command flags
+
+            Return:
+                If there are any command flags
+        """
+        if '--help' in self.message:
+            return True
+        elif '--why' in self.message:
+            return True
+        return False
+
+    def get_flag_response(self) -> str:
+        """
+            Get response from the entered flag
+        """
+        if '--help' in self.message:
+            return '\n'.join([
+                'Command     Description',
+                '--help      Shows this menu',
+                '--why       Why do I have these emojis',
+                '--why_all   What does every emoji mean?'])
+
+        elif '--why_all' in self.message:
+            s = "User specific reactions:\n"
+            for _, v in self.users.items():
+                s += v.explain()
+
+            s += "\nDate specific reactions:\n"
+            for _, v in self.days.items():
+                s += v.explain()
+
+            s += "\nMessage specific reactions:\n"
+            for _, v in self.message_emojis.items():
+                s += v.explain()
+
+            return s
+
+        elif '--why' in self.message:
+            lst = self.get_all_emoji_explanation()
+
+            s = ""
+            for emoji in lst:
+                s += emoji
+            return s if len(s) > 0 else "I couldn't find any reason you would have emojis\n¯\_(ツ)_/¯"
+
+        return ""
+
+    def get_all_emoji_explanation(self) -> list:
+        """
+            Gets the reasons for each emoji on the server
+
+            Return:
+                list of strings
+        """
+        lst = []
+        try:
+            lst += self.users[self.user].explain()
+        except Exception:
+            lst += []
+
+        try:
+            today = str(date.today())[5:]
+            lst += self.days[today].explain()
+        except Exception:
+            lst += []
+
+        if any(['games' in self.message, 'gamez' in self.message, 'gamz' in self.message]):
+            lst += self.message_emojis['games'].explain()
+
+        if self.contains_pen15():
+            lst += self.message_emojis['pen15'].explain()
+
+        return lst
+
 
     def contains_pen15(self) -> bool:
         """
