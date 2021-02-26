15b75bbc909268343f12ed3d893cadfaada92f3a
GeistInDerSH <geistindersh@gmail.com>
2021-02-25 21:25:00 -0700

    Added option to print if the user sends a flag, and for the bot to not react to its own messages

--- bot.py
+++ bot.py
@@ -11,8 +11,17 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-    reaction = Reaction(message.author.id, message.content)
-    for emoji in reaction.get_reactions():
-        await message.add_reaction(emoji)
+
+    if str(message.author.id) != '759317087192612864':
+        reaction = Reaction(message.author.id, message.content)
+
+        if reaction.contains_flags():
+            send_message = reaction.get_flag_response()
+
+            if len(send_message) > 0:
+                await client.get_channel(message.channel.id).send(send_message)
+        else:
+            for emoji in reaction.get_reactions():
+                await message.add_reaction(emoji)
 
 client.run(TOKEN)
